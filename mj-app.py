b51d3feec639c6556bafb5f66e9b5e3c02eea509
lakshitha <lakshitham022@gmail.com>
2025-06-08 19:50:05 +0530

    Update mj-app.py

--- mj-app.py
+++ mj-app.py
@@ -17,7 +17,7 @@ def analyze_sentiment(text):
     polarity = blob.sentiment.polarity
     if polarity > 0.2:
         return "Positive"
-    elif polarity < 0:
+    elif polarity < -0.2:
         return "Negative"
     else:
         return "Neutral"
